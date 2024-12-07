cd63078f0db942bf89d64922a657055ea9343c22
Victor de Camargo <victor@rhwd.dev>
2024-12-07 11:41:34 -0300

    move routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import watchPump from "./watchPump.js";
+import routes from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
 watchPump();
 
-app.get("/", (req, res) => {
-    res.send("sex");
-});
+app.use("/", routes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
