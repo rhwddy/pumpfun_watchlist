e8b24963622f16c2949e8f17841d04adb0aee5eb
Victor de Camargo <victor@rhwd.dev>
2024-12-07 19:14:45 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import watchPump from "./watchPump.js";
 import routes from "./routes/index.js";
 import createTables from "./config/createTables.js";
+import cors from "cors";
+import pool from "./config/db.js";
 
 const app = express();
 const port = 3000;
@@ -9,9 +11,20 @@ const port = 3000;
 await createTables();
 
 watchPump();
+app.use(cors());
+
+app.use(express.json());
 
 app.use("/", routes);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
+});
+
+process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+    pool.end();
+    server.close();
+    console.log("gracefully shut down.");
+    process.exit(0);
 });
